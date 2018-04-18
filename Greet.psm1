bbee0b6885cd132a85c1aab63bdb25e067c44c10
Lindmeier <Jan.Lindmeier@gmail.com>
2018-04-18 18:23:31 +0200

    Add files via upload

--- Greet.psm1
+++ Greet.psm1
@@ -3,10 +3,28 @@ function New-Greeting() {
     param(
         [Parameter(Mandatory=$true, 
             ValueFromPipeline=$true)]
-        [string]$Name
+        [string]$Name,
+        [string]$Greeting = "Hello"
     )
 
     process {
-        "Hello " + $Name
+         $Greeting + " " + $Name
     }
+}
+
+function Get-Name () {
+
+    [CmdletBinding()]
+    param(
+        
+        [switch]$long
+    )
+    if ($long.IsPresent)
+    {
+       return @( 'a', 'b', 'c', 'd', 'e')
+    }
+    else {
+       return  @( 'a', 'b')
+    }
+    
 }
